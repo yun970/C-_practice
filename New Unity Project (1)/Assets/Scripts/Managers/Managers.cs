1515e35bd0ec126419bb113ce5eb41c7993d6b5d
Yun <scc00057@gmail.com>
2022-02-10 03:00:16 +0900

    .

---
+++ New Unity Project (1)/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers instance; //static이라는 키워드의 특성상 유일성이 보장
+    public static Managers GetManagers() { init(); return instance; } // 유일한 매니저클래스의 인스턴스를 호출
+    // Start is called before the first frame update
+    void Start()
+    {
+        init();
+        //GameObject go = GameObject.Find("@Managers");
+        //instance = go.GetComponent<Managers>();
+
+        //instance = this
+        //c# 스크립트 파일인 managers를 들고있는 @managers라는 오브젝트에게 start()가 호출되면서 자기자신에게 this를 부여
+        //하지만 @managers가 여러개 있다면 유일성이 보장되지 않으므로 @Managers라는 이름과 동일한 오브젝트만 호출함으로써 유일성 부여
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    static void init()
+    {
+        if(instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" }; //유니티내의 실제 오브젝트 @Managers를 생성
+                go.AddComponent<Managers>(); //오브젝트 @Managers에 스크립트인 Managers를 컴포넌트로 부착
+            }
+            DontDestroyOnLoad(go);
+            instance = go.GetComponent<Managers>(); // Managers mg 인스턴스에 컴포넌트를 연결
+        }
+    }
+}
---
+++ New Unity Project (1)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+struct MyVector
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public float magnitude { get { return 0; } }
+    public MyVector normalized { get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); } }
+
+    public MyVector(float x, float y, float z)
+    {
+        this.x  = x ; this.y  = y ; this.z  = z ;
+    }
+
+    public static MyVector operator + (MyVector a, MyVector b)
+    {
+        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+    public static MyVector operator -(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+    public static MyVector operator *(MyVector a, float b)
+    {
+        return new MyVector(a.x * b, a.y * b, a.z * b);
+    }
+}
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] // 리플렉션 어트리뷰트 기능
+    float xpeed = 10.0f;
+    PlayerController player;
+    void Start()
+    {
+        //GameObject go = GameObject.Find("Player");
+        //player = go.GetComponent<PlayerController>();
+
+        MyVector to = new MyVector(10.0f, 0.0f, 0.0f);
+        MyVector from = new MyVector(5.0f, 0.0f, 0.0f);
+        MyVector dir = to - from;
+        // 방향벡터
+        // 1. 거리(크기)    magnitude
+        // 2. 실제 방향     normalized
+        dir = dir.normalized;
+
+        MyVector mewPos = from + dir * xpeed;
+
+
+
+    }
+
+    void Update()
+    {
+        // transform.TransformDirection()   
+        // Local -> World
+
+        // transform.InverseTransformDirection()
+        // World -> Local
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * xpeed);
+            //transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * xpeed);
+            //Vector3.forward 는 new Vectoc3(1.0f,0.0f,0.0f,0.0f)과 같음            
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.TransformDirection(Vector3.back * Time.deltaTime * xpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * xpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.TransformDirection(Vector3.left * Time.deltaTime * xpeed);
+        }
+    }
+}
